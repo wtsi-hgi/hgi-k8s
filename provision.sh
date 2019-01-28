d4de00e3228a6115bfa32a0f93e4e2d279cc5895
Christopher Harrison <ch12@sanger.ac.uk>
2019-01-28 13:57:41 +0000

    Flesh out provisioning script, so we can bring up and destroy clusters

--- provision.sh
+++ provision.sh
@@ -2,11 +2,52 @@
 
 # Provision K8s cluster with Terraform, per Kubespray documentation
 
-set -e
+set -eu
 
-source gitlab.rc  # n.b., Not checked in
-source activate.rc
-cd inventory/testing
-terraform init ../../cellgeni-kubespray/contrib/terraform/openstack
-terraform apply -var-file=cluster.tf ../../cellgeni-kubespray/contrib/terraform/openstack
-source deactivate
+WORK_DIR="$(dirname "$(readlink -fn "$0")")"
+TERRAFORM_PATH="${WORK_DIR}/cellgeni-kubespray/contrib/terraform/openstack"
+
+main() {
+  local inventory
+  local -i destroy=0
+
+  while (( $# )); do
+    case "$1" in
+      "--destroy")
+        destroy=1
+        ;;
+
+      *)
+        if [[ -e "inventory/$1/cluster.tf" ]]; then
+          inventory="$1"
+        fi
+        ;;
+    esac
+
+    shift
+  done
+
+  if [[ -z "${inventory+x}" ]]; then
+    >&2 echo "No inventory specified!"
+    >&2 echo "Usage: provision.sh [--destroy] INVENTORY_NAME"
+    exit 1
+  fi
+
+  source gitlab.rc  # n.b., Not checked in
+  source activate.rc
+  trap "source deactivate" EXIT
+
+  cd "inventory/${inventory}"
+
+  if ! (( destroy )); then
+    # Initialise and deploy
+    terraform init "${TERRAFORM_PATH}"
+    terraform apply -var-file=cluster.tf "${TERRAFORM_PATH}"
+
+  else
+    # Destroy cluster
+    terraform destroy -var-file=cluster.tf "${TERRAFORM_PATH}"
+  fi
+}
+
+main "$@"
