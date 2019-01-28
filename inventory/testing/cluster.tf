0374ad75bc1dc8bc358987b99d82a4107d8ead24
Christopher Harrison <ch12@sanger.ac.uk>
2019-01-28 13:17:06 +0000

    Reduced resource requirements to fit into tenancy

--- inventory/testing/cluster.tf
+++ inventory/testing/cluster.tf
@@ -11,7 +11,7 @@ ssh_user = "ubuntu"
 
 # 0|1 bastion nodes
 number_of_bastions = 1
-flavor_bastion = "8003"
+flavor_bastion = "8001" # o1.small
 
 # standalone etcds
 number_of_etcd = 0
@@ -21,22 +21,22 @@ number_of_k8s_masters = 0
 number_of_k8s_masters_no_etcd = 0
 number_of_k8s_masters_no_floating_ip = 3
 number_of_k8s_masters_no_floating_ip_no_etcd = 0
-flavor_k8s_master = "8003"
+flavor_k8s_master = "8001" # o1.small
 
 # nodes
 number_of_k8s_nodes = 0
 number_of_k8s_nodes_no_floating_ip = 4
-flavor_k8s_node = "2004"
+flavor_k8s_node = "2002" # m1.medium
 
 # GlusterFS
 # either 0 or more than one
 number_of_gfs_nodes_no_floating_ip = 2
-gfs_volume_size_in_gb = 2000
+gfs_volume_size_in_gb = 500
 # Container Linux does not support GlusterFS
 image_gfs = "xenial-isg-docker-c52f7acc02b0c11d41b174707e4c271b16f52996"
 # May be different from other nodes
 ssh_user_gfs = "ubuntu"
-flavor_gfs_node = "8003"
+flavor_gfs_node = "8002" # o1.medium
 
 # networking
 network_name = "testing"
