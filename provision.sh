20635e52f0b64d210cc0ec6da05739e42298a9e5
Christopher Harrison <ch12@sanger.ac.uk>
2019-01-28 12:46:11 +0000

    Script to run the Terraform provisioning

---
+++ provision.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Provision K8s cluster with Terraform, per Kubespray documentation
+
+set -e
+
+source gitlab.rc  # n.b., Not checked in
+source activate.rc
+cd inventory/testing
+terraform init ../../cellgeni-kubespray/contrib/terraform/openstack
+terraform apply -var-file=cluster.tf ../../cellgeni-kubespray/contrib/terraform/openstack
